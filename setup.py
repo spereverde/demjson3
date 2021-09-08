b5b932cf11c215a7b0c4f281e434076ac5d35201
Niels Mündler <n.muendler@tum.de>
2021-09-08 02:09:18 +0200

    Minor fixes, improve description

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 # Python package setup script        -*- coding: utf-8 -*-
 
 name = "demjson3"
-version = "3.0.1"
+version = "3.0.2"
 
 from setuptools import setup
 
-distmech = "setuptools"
+from codecs import open
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name=name,
@@ -16,19 +21,8 @@ setup(
     author="Deron Meranda",
     author_email="deron.meranda@gmail.com",
     description="encoder, decoder, and lint/validator for JSON (JavaScript Object Notation) compliant with RFC 7159",
-    long_description="""
-The "demjson" module, and the included "jsonlint" script, provide methods
-for encoding and decoding JSON formatted data, as well as checking JSON
-data for errors and/or portability issues.  The jsonlint command/script
-can be used from the command line without needing any programming.
-
-Although the standard Python library now includes basic JSON support
-(which it did not when demjson was first written), this module
-provides a much more comprehensive implementation with many features
-not found elsewhere.  It is especially useful for error checking or
-for parsing JavaScript data which may not strictly be valid JSON data.
-
-""".strip(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license="GNU LGPL 3.0",
     keywords=["JSON", "jsonlint", "JavaScript", "UTF-32"],
     platforms=[],
@@ -37,7 +31,9 @@ for parsing JavaScript data which may not strictly be valid JSON data.
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
     ],
